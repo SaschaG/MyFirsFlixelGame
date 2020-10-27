b47b227d785f0375259a0ca00c3b2d205959e38a
Sascha <sascha@Patricias-MBP-2.fritz.box>
2020-10-27 18:04:00 +0100

    finished hello world tutorial

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,27 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
-{
+{	
+	var text:FlxText;
 	override public function create()
 	{
 		super.create();
+		text = new FlxText(0,0,FlxG.width,"Hello World!");
+		text.setFormat(null,64,FlxColor.RED,FlxTextAlign.CENTER);
+		add(text);
+
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		text.y++;
+		if(text.y > FlxG.height)
+			text.y = -64;
 	}
 }
