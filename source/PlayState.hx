65374382bb407bf7f946bcb8c19bcf5d1168235a
Sascha <sascha@Patricias-MBP-2.fritz.box>
2020-10-29 09:31:31 +0100

    Finished Video 4 of tutorial series

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,9 +3,10 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
+import flixel.input.FlxKeyManager;
+import flixel.input.keyboard.FlxKeyList;
+import flixel.input.keyboard.FlxKeyboard;
 import flixel.tweens.FlxTween;
-import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {	
@@ -16,19 +17,33 @@ class PlayState extends FlxState
 
 		sprite = new FlxSprite();
 		sprite.loadGraphic(AssetPaths.Battleship_1x3_top__png);
-		sprite.x = 100;
-		sprite.y = 0;
+		sprite.x = FlxG.width/2 -sprite.width/2;
+		sprite.y = FlxG.height/2 -sprite.height/2;
 		add(sprite);     
 
-		FlxTween.tween(sprite, {x: FlxG.width - sprite.width,
-							y: FlxG.height - sprite.height,
-							angle: 360.0}, 5, {type: FlxTweenType.PINGPONG});
+		
 
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-		sprite.x +=100 * elapsed;
+		if(FlxG.keys.pressed.LEFT){
+			sprite.x-= 120 * elapsed;
+		}
+		if(FlxG.keys.pressed.RIGHT){
+			sprite.x+= 120 * elapsed;
+		}
+		if(FlxG.keys.pressed.UP){
+			sprite.y-= 120*elapsed;
+		}
+		if(FlxG.keys.pressed.DOWN){
+			sprite.y+= 120 * elapsed;
+		}
+
+		if(FlxG.keys.anyJustPressed([ ESCAPE, SPACE])){
+			sprite.x = FlxG.width/2 -sprite.width/2;
+			sprite.y = FlxG.height/2 -sprite.height/2;
+		}
 	}
 }
