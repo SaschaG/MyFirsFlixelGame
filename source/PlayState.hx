df2507adcf955a4e7d3c857fd52519d522008950
Sascha <sascha@Patricias-MBP-2.fritz.box>
2020-10-29 10:02:14 +0100

    Finished tutorial video 5

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,11 +6,13 @@ import flixel.FlxState;
 import flixel.input.FlxKeyManager;
 import flixel.input.keyboard.FlxKeyList;
 import flixel.input.keyboard.FlxKeyboard;
+import flixel.input.mouse.FlxMouseEventManager;
 import flixel.tweens.FlxTween;
 
 class PlayState extends FlxState
 {	
 	var sprite:FlxSprite;
+	var scaleFactor = 0.1;
 	override public function create()
 	{
 		super.create();
@@ -20,30 +22,24 @@ class PlayState extends FlxState
 		sprite.x = FlxG.width/2 -sprite.width/2;
 		sprite.y = FlxG.height/2 -sprite.height/2;
 		add(sprite);     
-
-		
-
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-		if(FlxG.keys.pressed.LEFT){
-			sprite.x-= 120 * elapsed;
+		
+		if(FlxG.mouse.overlaps(sprite)){
+			if(FlxG.mouse.pressed){
+				sprite.setPosition(FlxG.mouse.getPosition().x - sprite.width /2, FlxG.mouse.getPosition().y - sprite.height / 2);
+			}
 		}
-		if(FlxG.keys.pressed.RIGHT){
-			sprite.x+= 120 * elapsed;
-		}
-		if(FlxG.keys.pressed.UP){
-			sprite.y-= 120*elapsed;
-		}
-		if(FlxG.keys.pressed.DOWN){
-			sprite.y+= 120 * elapsed;
-		}
-
-		if(FlxG.keys.anyJustPressed([ ESCAPE, SPACE])){
+		if(FlxG.mouse.justReleasedRight){
 			sprite.x = FlxG.width/2 -sprite.width/2;
 			sprite.y = FlxG.height/2 -sprite.height/2;
+			sprite.scale.set(1,1);
+		}
+		if(FlxG.mouse.wheel !=0){
+			sprite.scale.add(FlxG.mouse.wheel * scaleFactor, FlxG.mouse.wheel * scaleFactor);
 		}
 	}
 }
